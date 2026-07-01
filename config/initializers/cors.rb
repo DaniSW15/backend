4b7bdb91f033b3b7723525d9b58ef5941ea3f24f
Daniel CG <daniel.cg15.tab@gmail.com>
2026-07-01 00:22:22 -0600

    fix: use presence to correctly handle empty FRONTEND_URL string in CORS initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["FRONTEND_URL"]&.split(",") || [ "http://localhost:5173", "http://127.0.0.1:5173" ]
+    origins ENV["FRONTEND_URL"].presence&.split(",") || [ "http://localhost:5173", "http://127.0.0.1:5173" ]
 
     resource "*",
       headers: :any,
